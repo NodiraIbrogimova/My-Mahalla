33326d328a23ca71ef46796e55973fb3bb63f6cc
Nodira <nodira.ibrogimova@gmail.com>
2020-07-26 19:44:18 +0500

    style: change header styles, home page

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -6,16 +6,16 @@ import {Component} from '@angular/core';
     styleUrls: ['home.page.scss']
 })
 export class HomePage {
-    categories = {
-        ecology: 'Ecology',
-        energy: 'Energy',
-        residence: 'Residence',
-        roads: 'Roads',
-        water: 'Water',
-        social: 'Social',
-        financial: 'Financial',
-        education: 'Education'
-    };
+    categories = [
+        {type: 'Ecology', img: 'assets/categories-icons/ecology-category.png'},
+        {type: 'Energy', img: 'assets/categories-icons/electricity-category.png'},
+        {type: 'Residence', img: 'assets/categories-icons/residence-category.png'},
+        {type: 'Roads', img: 'assets/categories-icons/road-category.png'},
+        {type: 'Water', img: 'assets/categories-icons/water-category.png'},
+        {type: 'Social', img: 'assets/categories-icons/social-category.png'},
+        {type: 'Financial', img: 'assets/categories-icons/financial-category.png'},
+        {type: 'Education', img: 'assets/categories-icons/education-category.png'},
+    ];
 
     constructor() {
     }
