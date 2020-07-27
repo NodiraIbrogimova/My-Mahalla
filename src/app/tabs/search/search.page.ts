312a00f4acdbc3610c3be360fa9666b64ece4514
Nodira <nodira.ibrogimova@gmail.com>
2020-07-27 16:08:35 +0500

    style: add searchbar to search page

--- src/app/tabs/search/search.page.ts
+++ src/app/tabs/search/search.page.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
     selector: 'app-search',
@@ -10,10 +10,23 @@ export class SearchPage {
     locations = [
         'Tashkent, Chilanzar district', 'St. Navoi, House 21, Bukhara', 'St. Al-Khorazmiy, House 5, Samarkand', 'St. Al-Fergani, Fergana',
         'Navoi, Navbahor tumani', 'Surkhandarya, Sariosiyo', 'Surkhandarya Region, Shargun', 'St. Nakkashlik, Chilanzar district, Tashkent',
-         'Mirobod district, Tashkent', 'Yangiobod district, Tashkent', 'Tashkent Region', 'Guliston', 'Angren', 'Bekobod', 'Khiva', 'Jizzakh',
+        'Mirobod district, Tashkent', 'Yangiobod district, Tashkent', 'Tashkent Region', 'Guliston', 'Angren', 'Bekobod', 'Khiva', 'Jizzakh',
     ];
 
+
     constructor() {
+    }
+
+    @HostListener('scroll', ['$event'])
+    onScroll(event) {
+        console.log('scrolling ');
+        let searchbar = document.getElementById('searchbar');
+        let sticky = searchbar.offsetTop;
+        if (window.pageYOffset > sticky) {
+            searchbar.classList.add('sticky');
+        } else {
+            searchbar.classList.remove('sticky');
+        }
     }
 
 }
