9b88c2703cfbd1fb834768b494a63aff5402bb76
Nodira <nodira.ibrogimova@gmail.com>
2020-07-27 09:25:27 +0500

    feat: create search page

--- src/app/tabs/search/search.page.ts
+++ src/app/tabs/search/search.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: 'search.page.html',
-  styleUrls: ['search.page.scss']
+    selector: 'app-search',
+    templateUrl: 'search.page.html',
+    styleUrls: ['search.page.scss']
 })
-export class SearchPage{
+export class SearchPage {
 
-  constructor() {}
+    locations = [
+        'Tashkent, Chilanzar district', 'St. Navoi, House 21, Bukhara', 'St. Al-Khorazmiy, House 5, Samarkand', 'St. Al-Fergani, Fergana',
+        'Navoi, Navbahor tumani', 'Surkhandarya, Sariosiyo', 'Surkhandarya Region, Shargun', 'St. Nakkashlik, Chilanzar district, Tashkent',
+         'Mirobod district, Tashkent', 'Yangiobod district, Tashkent', 'Tashkent Region', 'Guliston', 'Angren', 'Bekobod', 'Khiva', 'Jizzakh',
+    ];
+
+    constructor() {
+    }
 
 }
