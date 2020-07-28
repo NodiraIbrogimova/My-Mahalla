e283eb865c2568e0ce9c64aaa5bd567f5da1ca2d
Nodira <nodira.ibrogimova@gmail.com>
2020-07-28 07:51:20 +0500

    refactor: remove unused hostlistener in search

--- src/app/tabs/search/search.page.ts
+++ src/app/tabs/search/search.page.ts
@@ -13,20 +13,6 @@ export class SearchPage {
         'Mirobod district, Tashkent', 'Yangiobod district, Tashkent', 'Tashkent Region', 'Guliston', 'Angren', 'Bekobod', 'Khiva', 'Jizzakh',
     ];
 
-
     constructor() {
     }
-
-    @HostListener('scroll', ['$event'])
-    onScroll(event) {
-        console.log('scrolling ');
-        let searchbar = document.getElementById('searchbar');
-        let sticky = searchbar.offsetTop;
-        if (window.pageYOffset > sticky) {
-            searchbar.classList.add('sticky');
-        } else {
-            searchbar.classList.remove('sticky');
-        }
-    }
-
 }
