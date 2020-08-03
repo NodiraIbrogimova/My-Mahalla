e22e0cc36ff90968663812894e452b7d19ba502c
Nodira <nodira.ibrogimova@gmail.com>
2020-08-03 19:50:16 +0500

    get values of form, submit with login

--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -11,70 +11,35 @@ import {UsernameValidator} from '../validators/username';
     styleUrls: ['./sign-in.page.scss']
 })
 export class SignInPage {
-    signInForm: any;
-
     @ViewChild('signUpSlider') signUpSlider;
 
-    public slideOneForm: FormGroup;
-    public slideTwoForm: FormGroup;
-
+    public registerForm: FormGroup;
     images = ['assets/photos/logo.png', 'assets/photos/communication.png', 'assets/photos/communicate-problems.png'];
 
-    public submitAttempt: boolean = false;
+    public submitAttempt = false;
 
     constructor(private router: Router, private navController: NavController, private formBuilder: FormBuilder) {
 
-        this.slideOneForm = formBuilder.group({
+        this.registerForm = formBuilder.group({
             firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
             lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
             age: ['', AgeValidator.isValid],
             username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')]), UsernameValidator.checkUsername],
-            privacy: ['', Validators.required],
             bio: ['']
         });
-        // this.signInForm = this.formBuilder.group({
-        //     firstName: new FormControl(['', Validators.required, Validators.min(3)]),
-        //     lastName: new FormControl(['', Validators.required, Validators.min(3)]),
-        //     phoneNumber: new FormControl([null, [Validators.required, Validators.pattern(new RegExp('(99)[0-9 ]{10}'))]])
-        // })
-    }
-
-    logForm() {
-        console.log(this.signInForm);
-    }
-
-    next(slider) {
-        slider.slideNext();
-    }
-
-    prev(slider) {
-        slider.slidePrev();
     }
 
     save() {
-
         this.submitAttempt = true;
 
-        if (!this.slideOneForm.valid) {
-            this.signUpSlider.slideTo(0);
-        }
-        else if (!this.slideTwoForm.valid) {
-            this.signUpSlider.slideTo(1);
+        if (!this.registerForm.valid) {
+            console.log('Please fill the form');
         }
         else {
             console.log('success!');
-            console.log(this.slideOneForm.value);
-            console.log(this.slideTwoForm.value);
+            console.log(this.registerForm.value);
+            this.navController.navigateRoot(`app/tabs/home`);
         }
 
-    }
-
-    navigateToProfile() {
-        // if (this.signInForm.dirty && this.signInForm.valid) {
-        //     console.log('success');
-        //     this.navController.navigateRoot(`app/tabs/home`);
-        // }
-        // console.log('error');
-        this.navController.navigateRoot(`app/tabs/home`);
     }
 }
